d34a042451f836ca0c48df02d70303c9d657a459
mmokhi <mokhi64@gmail.com>
2017-05-31 18:34:34 +0430

    rename package

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ import os
 # python setup.py install   
 
 setup(
-	name = 'gmailfs',
+	name = 'gmailfs-fuse',
 	version = '0.8.6',
 	author = 'Mahdi Mokhtari',
 	url = 'https://github.com/mmokhi/gmailfs',
-	packages = ['gmailfs'],
-	package_dir = {'gmailfs':'src/gmailfs/'},
+	packages = ['gmailfs-fuse'],
+	package_dir = {'gmailfs-fuse':'src/gmailfs/'},
 	scripts = ['src/gmailfs/mount_gmailfs'],
 	data_files = [('/usr/local/etc/gmailfs/', ['src/gmailfs/conf/gmailfs.conf'])],
 )
